541320215c5d538de63227038913e2612f688078
carajillu <joan.clark88@gmail.com>
2026-07-14 17:44:19 +0100

    Made PLM packages more modular. can probably still condense into BERT and ESM, but no need for now

--- adc_developability/proteins/facebook/esm2_t33_650M.py
+++ adc_developability/proteins/facebook/esm2_t33_650M.py
@@ -1,65 +1,23 @@
 from transformers import EsmModel, EsmTokenizer, EsmForMaskedLM, pipeline
-import re
+from adc_developability.proteins.utils.sequence_prep import seq_prep
+from adc_developability.proteins.utils.masking import masker, unmasker
+from adc_developability.proteins.utils.features import featurizer
 import pandas as pd
-import numpy as np
-import random
-import torch
 
-def esm2_prep(sequence: str|list[str]) -> list[str]:
-    if type(sequence)==str:
-        sequence=[sequence]
-    sequence=[seq.upper() for seq in sequence] # Upper case
-    sequence = [re.sub(r"[BXZJUO]", "X", seq) for seq in sequence] # Replace rare AAs with X (not sure if relevant for this model)
-    return sequence
+TOKENIZER = EsmTokenizer.from_pretrained("facebook/esm2_t33_650M_UR50D", do_lower_case=False)
+MODEL = EsmModel.from_pretrained("facebook/esm2_t33_650M_UR50D")
+MODEL_MLM = EsmForMaskedLM.from_pretrained("facebook/esm2_t33_650M_UR50D")
+PIPELINE = pipeline('fill-mask', model=MODEL_MLM, tokenizer=TOKENIZER)
 
-def get_esm2_features(sequence: str|list[str]):
-    tokenizer = EsmTokenizer.from_pretrained("facebook/esm2_t33_650M_UR50D", do_lower_case=False)
-    model = EsmModel.from_pretrained("facebook/esm2_t33_650M_UR50D")
-    sequence=esm2_prep(sequence)
-    encoded_input = tokenizer(sequence, return_tensors='pt')
-    output = model(**encoded_input)
-    return output
-    
 def get_esm2_df(sequence: str|list[str]):
-    features=get_esm2_features(sequence)
+    features=featurizer(sequence, TOKENIZER, MODEL)
     df=pd.DataFrame(features[0].flatten().detach().numpy()).transpose()
     df.columns=[f"esm2_{i}" for i in range(df.shape[1])]
     return df
 
-def masker(seq_str: str, mask_fraction: float=0.15, seed: int=42) -> pd.DataFrame:
-    seq_lst=" ".join(seq_str).split()
-    if mask_fraction==1:
-        mask_positions=range(len(seq_lst))
-    else:
-        n_mask=max(1, int(len(seq_lst) * mask_fraction))
-        mask_positions = random.sample(range(len(seq_lst)),n_mask)
-    masked_seqs=[]
-    for position in mask_positions:
-        masked_seqs.append(
-            {"masked_position":position,
-             "masked_residue":seq_lst[position],
-             "masked_sequence":" ".join(["<mask>" if i == position else aa for i, aa in enumerate(seq_lst)]),
-             }
-        )
-    return pd.DataFrame(masked_seqs)
-
-
 def esm2_unmask(sequence:str|list[str],mask_fraction:float=0.15,seed:int=42):
-    tokenizer = EsmTokenizer.from_pretrained("facebook/esm2_t33_650M_UR50D", do_lower_case=False )
-    model = EsmForMaskedLM.from_pretrained("facebook/esm2_t33_650M_UR50D")
-    sequence=esm2_prep(sequence)
-    df=masker(sequence[0],mask_fraction=mask_fraction,seed=seed)
-    for i in range(1,len(sequence)):
-        df=pd.concat([df,masker(sequence[0],mask_fraction=mask_fraction,seed=seed)])
-    unmasker = pipeline('fill-mask', model=model, tokenizer=tokenizer)
-    unmasked_lst=df.masked_sequence.apply(unmasker)
-    unmasked_df=[]
-    for preds in unmasked_lst:
-        row = {}
-        for j, pred in enumerate(preds):
-            row[f"pred_{j}"] = pred["token_str"]
-            row[f"score_{j}"] = pred["score"]
-        unmasked_df.append(row)
-    unmasked_df=pd.DataFrame(unmasked_df)
-    unmasked_df=pd.concat([df,unmasked_df],axis=1)
-    return unmasked_df
+    sequence=seq_prep(sequence)
+    masked=pd.concat([masker(sequence[i], mask_str="<mask>", mask_fraction=mask_fraction, seed=seed) for i in range(0,len(sequence))],ignore_index=True)
+    unmasked_df=unmasker(masked.masked_sequence.tolist(), PIPELINE)
+    df=pd.concat([masked,unmasked_df],axis=1)
+    return df
--- adc_developability/proteins/zjunlp/onto_protein.py
+++ adc_developability/proteins/zjunlp/onto_protein.py
@@ -1,68 +1,23 @@
 from transformers import BertModel, BertTokenizer, BertForMaskedLM, pipeline
-import re
+from adc_developability.proteins.utils.sequence_prep import seq_prep
+from adc_developability.proteins.utils.masking import masker, unmasker
+from adc_developability.proteins.utils.features import featurizer
 import pandas as pd
-import numpy as np
-import random
-import torch
 
+TOKENIZER = BertTokenizer.from_pretrained("zjunlp/OntoProtein", do_lower_case=False)
+MODEL = BertModel.from_pretrained("zjunlp/OntoProtein")
+MODEL_MLM = BertForMaskedLM.from_pretrained("zjunlp/OntoProtein")
+PIPELINE = pipeline('fill-mask', model=MODEL_MLM, tokenizer=TOKENIZER)
 
-def onto_protein_prep(sequence: str|list[str]) -> list[str]:
-    if type(sequence)==str:
-        sequence=[sequence]
-    sequence=[seq.upper() for seq in sequence] # Upper case
-    sequence=[" ".join(seq) for seq in sequence] # Separate by spaces
-    sequence = [re.sub(r"[BXZJUO]", "X", seq) for seq in sequence] # Replace rare AAs with X
-    return sequence
-
-def get_onto_protein_features(sequence: str|list[str]):
-    tokenizer = BertTokenizer.from_pretrained("zjunlp/OntoProtein", do_lower_case=False)
-    model = BertModel.from_pretrained("zjunlp/OntoProtein")
-    sequence=onto_protein_prep(sequence)
-    encoded_input = tokenizer(sequence, return_tensors='pt')
-    output = model(**encoded_input)
-    return output
-    
 def get_onto_protein_df(sequence: str|list[str]):
-    features=get_onto_protein_features(sequence)
+    features=featurizer(sequence, TOKENIZER, MODEL)
     df=pd.DataFrame(features[0].flatten().detach().numpy()).transpose()
     df.columns=[f"onto_protein_{i}" for i in range(df.shape[1])]
     return df
 
-def masker(seq_str: str, mask_fraction: float=0.15, seed: int=42) -> pd.DataFrame:
-    seq_lst=" ".join(seq_str).split()
-    if mask_fraction==1:
-        mask_positions=range(len(seq_lst))
-    else:
-        n_mask=max(1, int(len(seq_lst) * mask_fraction))
-        mask_positions = random.sample(range(len(seq_lst)),n_mask)
-    masked_seqs=[]
-    for position in mask_positions:
-        masked_seqs.append(
-            {"masked_position":position,
-             "masked_residue":seq_lst[position],
-             "masked_sequence":" ".join(["[MASK]" if i == position else aa for i, aa in enumerate(seq_lst)]),
-             }
-        )
-    return pd.DataFrame(masked_seqs)
-
-
 def onto_protein_unmask(sequence:str|list[str],mask_fraction:float=0.15,seed:int=42):
-    tokenizer = BertTokenizer.from_pretrained("zjunlp/OntoProtein", do_lower_case=False )
-    model = BertForMaskedLM.from_pretrained("zjunlp/OntoProtein")
-    sequence=onto_protein_prep(sequence)
-    df=masker(sequence[0],mask_fraction=mask_fraction,seed=seed)
-    for i in range(1,len(sequence)):
-        df=pd.concat([df,masker(sequence[0],mask_fraction=mask_fraction,seed=seed)])
-    unmasker = pipeline('fill-mask', model=model, tokenizer=tokenizer)
-    unmasked_lst=df.masked_sequence.apply(unmasker)
-    unmasked_df=[]
-    for preds in unmasked_lst:
-        row = {}
-        for j, pred in enumerate(preds):
-            row[f"pred_{j}"] = pred["token_str"]
-            row[f"score_{j}"] = pred["score"]
-        unmasked_df.append(row)
-    unmasked_df=pd.DataFrame(unmasked_df)
-    unmasked_df=pd.concat([df,unmasked_df],axis=1)
-    return unmasked_df
-
+    sequence=seq_prep(sequence)
+    masked=pd.concat([masker(sequence[i], mask_str="[MASK]", mask_fraction=mask_fraction, seed=seed) for i in range(0,len(sequence))],ignore_index=True)
+    unmasked_df=unmasker(masked.masked_sequence.tolist(), PIPELINE)
+    df=pd.concat([masked,unmasked_df],axis=1)
+    return df
