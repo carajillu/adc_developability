f34450c9029818c437079f0198484957d80581d9
carajillu <joan.clark88@gmail.com>
2026-07-16 11:29:20 +0100

    using torch.no_grad()

--- adc_developability/proteins/utils/features.py
+++ adc_developability/proteins/utils/features.py
@@ -1,8 +1,10 @@
 
 from adc_developability.proteins.utils.sequence_prep import seq_prep
+import torch
 
 def featurizer(sequence: str|list[str],tokenizer,model):
     sequence=seq_prep(sequence)
     encoded_input = tokenizer(sequence, return_tensors='pt')
-    output = model(**encoded_input)
+    with torch.no_grad():
+        output = model(**encoded_input)
     return output
