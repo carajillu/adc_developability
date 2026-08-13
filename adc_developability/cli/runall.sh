e217fb7a3c87d67377e4c005199dec3448a54ea4
carajillu <joan.clark88@gmail.com>
2026-08-13 11:21:56 +0100

    bash script to run all combinations of models

---
+++ adc_developability/cli/runall.sh
@@ -0,0 +1,17 @@
+python adc_developability/cli/full_embedding.py  --input .data/ADCdb_processed.csv  --plm adc_developability.proteins.rostlab.prot_bert  --abplm adc_developability.antibody_Fv.antiberty_utils  --smllm adc_developability.wlp.deepchem.chemberta  --output protbert_chemberta.csv --scheme chothia 
+
+python adc_developability/cli/full_embedding.py  --input .data/ADCdb_processed.csv  --plm adc_developability.proteins.rostlab.prot_bert  --abplm adc_developability.antibody_Fv.antiberty_utils  --smllm adc_developability.wlp.juim.smiles_bert  --output protbert_smiles-bert.csv --scheme chothia 
+
+python adc_developability/cli/full_embedding.py  --input .data/ADCdb_processed.csv  --plm adc_developability.proteins.rostlab.prot_bert  --abplm adc_developability.antibody_Fv.antiberty_utils  --smllm adc_developability.wlp.unikei.bert_base_smiles  --output protbert_bert-base-smiles.csv --scheme chothia 
+
+python adc_developability/cli/full_embedding.py  --input .data/ADCdb_processed.csv  --plm adc_developability.proteins.facebook.esm2_t33_650M  --abplm adc_developability.antibody_Fv.antiberty_utils  --smllm adc_developability.wlp.deepchem.chemberta  --output esm2_chemberta.csv --scheme chothia 
+
+python adc_developability/cli/full_embedding.py  --input .data/ADCdb_processed.csv  --plm adc_developability.proteins.facebook.esm2_t33_650M  --abplm adc_developability.antibody_Fv.antiberty_utils  --smllm adc_developability.wlp.juim.smiles_bert  --output esm2_smiles-bert.csv --scheme chothia 
+
+python adc_developability/cli/full_embedding.py  --input .data/ADCdb_processed.csv  --plm adc_developability.proteins.facebook.esm2_t33_650M  --abplm adc_developability.antibody_Fv.antiberty_utils  --smllm adc_developability.wlp.unikei.bert_base_smiles  --output esm2_bert-base-smiles.csv --scheme chothia 
+
+python adc_developability/cli/full_embedding.py  --input .data/ADCdb_processed.csv  --plm adc_developability.proteins.yarongef.distillprotbert  --abplm adc_developability.antibody_Fv.antiberty_utils  --smllm adc_developability.wlp.deepchem.chemberta  --output distillprotbert_chemberta.csv --scheme chothia 
+
+python adc_developability/cli/full_embedding.py  --input .data/ADCdb_processed.csv  --plm adc_developability.proteins.yarongef.distillprotbert  --abplm adc_developability.antibody_Fv.antiberty_utils  --smllm adc_developability.wlp.juim.smiles_bert  --output distillprotbert_smiles-bert.csv --scheme chothia 
+
+python adc_developability/cli/full_embedding.py  --input .data/ADCdb_processed.csv  --plm adc_developability.proteins.yarongef.distillprotbert  --abplm adc_developability.antibody_Fv.antiberty_utils  --smllm adc_developability.wlp.unikei.bert_base_smiles  --output distillprotbert_bert-base-smiles.csv --scheme chothia 
